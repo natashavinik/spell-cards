121fe9dfc9e4fbe73a21a27497d7f3fc3ff639e2
Kat Berton <katherine.berton@gmail.com>
2024-04-27 10:44:57 -0700

    spell card styling with wraps and stuff

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { fetchSpells } from './fetchSpells.jsx'
+import { SpellCard } from './components.jsx'
 import './App.css'
 
 export const App = () => {
   const { data, isLoading } = useQuery(['fetchSpells'], fetchSpells)
-
   return <> 
       <h1>Spells</h1>
-      <div display="flex" className="card" >
-        {isLoading ? 'there are not spells' : 'there are spells'}
+      <div className="flex-container" >
+        {!isLoading && data.results.map(spell => <SpellCard spell={spell} />)}
       </div>
     </>
   
@@ -17,6 +17,3 @@ export const App = () => {
 
 
 
-const SpellCard = ({spell}) => {
-  return <p key={spell.slug}>{spell.name}</p>
-}
