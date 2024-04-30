605467c0e19bbd2e89e60a0e7e21a45d3d6a08d6
Natasha Vinik <natasha.n.vinik@gmail.com>
2024-04-30 12:00:20 -0700

    finished exercise 2a

--- my-vue-app/src/App.jsx
+++ my-vue-app/src/App.jsx
@@ -1,17 +1,23 @@
-import { useQuery } from '@tanstack/react-query'
-import { fetchSpells } from './fetchSpells.jsx'
-import { useState } from 'react'
-import { DetailSidebar } from './components.jsx'
-import './App.css'
+import { useQuery } from "@tanstack/react-query";
+import { fetchSpells } from "./fetchSpells.jsx";
+import { useState } from "react";
+import { DetailSidebar } from "./components.jsx";
+import { SpellCard } from "./components.jsx";
+import "./App.css";
 
 export const App = () => {
-  const { data, isLoading } = useQuery(['fetchSpells'], fetchSpells)
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
-  return <> 
+  const { data, isLoading } = useQuery(["fetchSpells"], fetchSpells);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  return (
+    <>
       <h1>Spells</h1>
-      <div className="flex-container" >
-        { isLoading ? 'there are no spells' : 'there are spells'}
+      <div className="flex-container">
+        {!isLoading &&
+          data.results.map((spell) => {
+            return <SpellCard spell={spell} />;
+          })}
       </div>
       <DetailSidebar isOpen={isSidebarOpen} />
     </>
-}
+  );
+};
